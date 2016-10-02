c394aa1d30347c01e6c9dc6f8fe6be949efeb2d8
JamesDarrenDavis <support.omniscope@gmail.com>
2016-10-02 16:56:35 -0400

    Added Script

---
+++ Assets/Scripts/MyScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("My First Git Project");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
